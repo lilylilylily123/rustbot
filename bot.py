da7fc9695729fcc19613c950394d275fd42f9e5f
lily <102743831+lilylilylily123@users.noreply.github.com>
2022-10-05 22:43:44 +0200

    messed around w/ dm functionality, doesn't work atm but will fix asap

--- bot.py
+++ bot.py
@@ -37,12 +37,34 @@ async def on_message(message):
         file = discord.File("map.png", filename="image.png")
         await message.channel.send(file=file, embed=embed)
         os.remove('map.png')
+
     if message.content.startswith('?players'):
         server = await socket.get_info()
         players = (await socket.get_info()).players
         embed = discord.Embed(title=f"Player Count", color=discord.colour.Color.dark_purple(), description=f"*{server.name}*\n \n There are **{players}** players online.")
         await message.channel.send(embed=embed)
+
     if message.content.startswith('?server'):
+        map = await socket.get_map(True, True, True)
+        server = await socket.get_info()
+        players = (await socket.get_info()).players
+        map.save('map.png')
+        time = (await socket.get_time()).time
+        embed = discord.Embed(title=f"**Server Info**", color=discord.colour.Color.dark_purple(), description=f"*{server.name}*\n \n There are **{players}** players online, with the time being **{time}**.")
+        file = discord.File("map.png", filename="image.png")
+        await message.channel.send(file=file, embed=embed)
+        os.remove('map.png')
+
+@client.event
+async def dm(message, member):
+    if message.content.startswith('?playerdetails'):
+        playertoken = socket.player_token
+        serverip = socket.ip
+        serverport = socket.port
+        steamid = socket.steam_id
+        await create_dm(user)
+        await dm_channel.send(f'{playertoken} {serverip} {serverport} {steamid}')
+
 client.run(TOKEN)
 
 
