dfb38759704e0a2f418aefbc075a55c6f22c5904
lily <102743831+lilylilylily123@users.noreply.github.com>
2022-10-05 21:52:23 +0200

    added more command functionality

--- bot.py
+++ bot.py
@@ -12,15 +12,6 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-
-async def time():
-    print(f"It is {(await socket.get_time()).time}")
-
-
-async def serverinfo():
-    print(f"{(await socket.get_info()).players}")
-
-
 @client.event
 async def on_ready():
     print(f'connected as: {client.user}')
@@ -32,11 +23,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('gerard'):
+        await message.channel.send('is gay!!!!')
 
-    if message.content.startswith('$players'):
+    if message.content.startswith('storm'):
+        await message.channel.send('is lesbian!!!!')
+
+    if message.content.startswith('?map'):
+        map = await socket.get_map(True, True, True)
+        server = await socket.get_info()
+        map.save('map.png')
+        embed = discord.Embed(title=f"Server Map", color=discord.colour.Color.dark_purple(), description=f"{server.name}")
+        file = discord.File("map.png", filename="image.png")
+        await message.channel.send(file=file, embed=embed)
+        os.remove('map.png')
+    if message.content.startswith('?players'):
+        server = await socket.get_info()
         players = (await socket.get_info()).players
-        await message.channel.send(f"There are {players} players online!")
-
+        embed = discord.Embed(title=f"Player Count", color=discord.colour.Color.dark_purple(), description=f"*{server.name}*\n \n There are **{players}** players online.")
+        await message.channel.send(embed=embed)
+    if message.content.startswith('?server'):
 client.run(TOKEN)
+
+
+
