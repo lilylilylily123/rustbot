8777d6f5b9911e8c81e4e8a8869b81bfd24081b4
lily <102743831+lilylilylily123@users.noreply.github.com>
2022-10-13 16:57:20 +0200

    COMPLETELY rewrote everything in interactions.py for proper slash commands.

--- bot.py
+++ bot.py
@@ -6,9 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = interactions.Client(token=TOKEN)
-
+@bot.event()
+async def on_ready():
+    print(f"Connected.")
+@bot.event()
+async def on_guild_member_add(ctx: interactions.GuildMember):
+    await ctx.send(f"Welcome!")
 @bot.command(name='ping', description="Send the ping of the bot.")
 async def ping(ctx: interactions.CommandContext):
-    await ctx.send(f"Pong! {bot.latency.__round__(1)} ms.")
+    await ctx.send(f"Pong! {bot.latency.__round__(0)} ms.")
 
 bot.start()
